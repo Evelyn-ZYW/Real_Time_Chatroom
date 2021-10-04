5c15932710fcc033499cb28a51d3d5171c3627fe
Evelyn Z <evelynzdragon@gmail.com>
2021-10-04 12:23:01 -0700

    1. created the Chatroom class 2. created the addChat methods

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,1 +1,30 @@
 // responsible for getting all the chat and data together
+
+// adding new chat documents (new messages - called 'document' in firebase)
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+  constructor(username, room){
+    this.username = username;
+    this.room = room;
+    this.chats = db.collection('chat');
+  }
+  async addChat(message){
+    //format a chat object
+    const now = new Date();
+    const chat = {
+      message: message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    //save the chat document
+    const response = await this.chats.add(chat);
+    return response;
+  }
+}
+
+const chatroom = new Chatroom('gaming', 'shaun');
+console.log(chatroom);
